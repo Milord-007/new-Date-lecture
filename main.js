d54bc9a46c02da8cf4a760ca6f5c8ad6c2a7ab9c
Ardasher <misterardasher@gmail.com>
2023-05-25 12:29:50 +0500

    set pic example added

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-let currentDate = new Date()
-let seconds = currentDate.getSeconds();
-console.log(seconds);// shows current seconds
+let date = new Date();
+date.setFullYear(2022);
+date.setMonth(11); // 11 - декабрь
+date.setDate(25);
+date.setHours(12);
+date.setMinutes(30);
+date.setSeconds(0);
+date.setMilliseconds(0);
+console.log(date); // Sun Dec 25 2022 12:30:00 GMT+0300
